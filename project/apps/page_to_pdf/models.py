82cfcbb9a3ebecd3d0d30acfb2ca4bc8833bdab7
ThomasNicholas21 <thomasnicholaas@gmail.com>
2025-12-08 22:01:50 -0300

    feat: implementing model Url and implementing manytomany Page

--- project/apps/page_to_pdf/models.py
+++ project/apps/page_to_pdf/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
 
+class Url(models.Model):
+    name = models.CharField("Name", max_length=512)
+
+    class Meta:
+        verbose_name = "Url"
+        verbose_name_plural = "Urls"
+
+    def __str__(self):
+        return self.name
+
+
 class Page(models.Model):
     class UrlTypeChoice(models.TextChoices):
         DOCUMENT = "document", "Document"
@@ -8,5 +19,12 @@ class Page(models.Model):
         OTHER = "other", "Other"
 
     name = models.CharField("Name")
-    url = models.URLField("URL Field")
     url_type = models.CharField("URL type", choices=UrlTypeChoice.choices)
+    url = models.ManyToManyField(Url, verbose_name="Url", related_name="page")
+
+    class Meta:
+        verbose_name = "Page"
+        verbose_name_plural = "Pages"
+
+    def __str__(self):
+        return self.name
